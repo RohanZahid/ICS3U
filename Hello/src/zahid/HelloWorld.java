1ac3f63025a1d6253a0f72d4d5f3d8f57d4b91b4
Rohan Zahid <323931535@C-H63W3V1.tdsb.on.ca>
2017-02-21 14:45:32 -0500

    Pushed "Hello, World!" program.

---
+++ Hello/src/zahid/HelloWorld.java
@@ -0,0 +1,15 @@
+package zahid;
+/**
+ * HelloWorld.java
+ * Outputs "Hello, World!" to the console.
+ * February 21, 2017
+ * @author Rohan Zahid
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
