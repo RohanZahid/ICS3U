526a45a4faa53a4d4d3065b72a422b3b2315a9af
Rohan Zahid <323931535@C-H63W3V1.tdsb.on.ca>
2017-04-10 14:03:57 -0400

    Done basic version.
    Periods and Capital letters have not been implemented

--- Unit 2/src/zahid/ReverseSentence.java
+++ Unit 2/src/zahid/ReverseSentence.java
@@ -1,57 +1,23 @@
 package zahid;
 
 import java.util.Scanner;
-
+/**
+ * ReverseSentence.java
+ * Monday, April 4th, 2017
+ * Prints a sentence with all words backward.
+ * @author Rohan Zahid
+ */
 public class ReverseSentence {
 
 	public static void main(String[] args) {
 		Scanner scan= new Scanner (System.in);
 		System.out.println("Enter a sentence to get an output with all words written backwards.");
 		String [] myString = scan.nextLine().split(" ");
-		//String s = " " + myString[0];
-		//String output = input.substring(0, 1).toUpperCase() + input.substring(1); 
 		for (int i = 0; i < myString.length; i++) {
-			int x = myString.length-1;
 			String s = " " + myString[i];
-			String b = myString[i];
-			/*if (i == 0) {
-				String c= b.substring(0, 1).toLowerCase();
-				for (int j = s.length()-1; j>0; j--) {
-					System.out.print((s.charAt(j) + c));
-				}
-			}
-			else if (i == x) {
-				String c= b.substring(0, b.length()-1).toUpperCase();
-				for (int j = s.length()-1; j>0; j--) {
-					System.out.print((c + s.charAt(j) + "."));
-				}
-			}
-			else if (myString[i].contains(".")) {
-				for (int j = s.length()-2; j>=0; j--) {
-					System.out.print((s.charAt(j)));
-				}
-			}
-			else {*/
 			for (int j = s.length()-1; j>=0; j--) {
-				if (s.contains(".")) {
-					s.split(".");
-					for (int j = s.length()-1; j>0; j--) {
-						System.out.print((s.charAt(j) + c));
-					}
-				}
-				else if(i == 0) {
-					String c= b.substring(0, 1).toLowerCase();
-					for (int j = s.length()-1; j>0; j--) {
-						System.out.print((s.charAt(j) + c));
-					}
 				System.out.print((s.charAt(j)));
-			//}
 			}
-				else if (i == x) {
-					String c= b.substring(0, b.length()-1).toUpperCase();
-					for (int j = s.length()-1; j>0; j--) {
-						System.out.print((c + s.charAt(j) + "."));
-					}
 		}
 
 	}
